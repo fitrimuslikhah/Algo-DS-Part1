0df7c9e103754b82cf1b872e24150c1f45a36403
fitri <fitrimuslikhah7@gmail.com>
2024-05-18 21:11:40 +0700

    problem 5

--- problem5/main.py
+++ problem5/main.py
@@ -1,5 +1,15 @@
 def max_sequence(arr):
-    return 0
+    max_current = max_global = arr[0]
+
+    for num in range(1, len(arr)):
+        max_current = max(arr[num], max_current + arr[num])
+        if max_current > max_global:
+            max_global = max_current
+    
+    return max_global
+
+    
+print(max_sequence)
 
 if __name__ == "__main__":
     print(max_sequence([-2, 1, -3, 4, -1, 2, 1, -5, 4]))  # 6
